abcce5da91b261553f358c4184d2ab2195c0f8b0
Ben <benjx32@gmail.com>
2022-01-20 16:21:38 -0800

    test 3 fixed, now does not include images

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,14 +8,14 @@ import java.util.Arrays;
 /*
     Test 2 failed because of improper bracket syntax.
 
-    Test 3 includes images
+    Test 3 includes images, now it doesn't
 */
 
 public class MarkdownParse {
+    static String[] imgExt = {".png",".jpeg",".gif", ".csv", ".jpg", ".svg", ".pdf"};
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         
-        String[] imgExt = {".png",".jpeg",".gif", ".csv", ".jpg", ".svg", ".pdf"};
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
@@ -32,14 +32,21 @@ public class MarkdownParse {
             openParen = markdown.indexOf("(", nextCloseBracket);
             closeParen = markdown.indexOf(")", openParen);
             String appendString = markdown.substring(openParen + 1, closeParen);
-            toReturn.add(appendString);
+            if(!containsExtension(appendString)){
+                toReturn.add(appendString);
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
     }
 
-    public boolean containsImgExt(String appendString){
-        return true;
+    public static boolean containsExtension(String appendString){
+        for(String i : imgExt){
+            if(appendString.contains(i)){
+                return true;
+            }
+        }
+        return false;
     }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
