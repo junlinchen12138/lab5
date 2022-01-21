87cbdb808b5707aae1d89ae3f88f1108908b0075
Ben <benjx32@gmail.com>
2022-01-20 16:08:51 -0800

    test 2: invalid link syntax fixed

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,23 +3,43 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+    Test 2 failed because of improper bracket syntax.
+
+    Test 3 includes images
+*/
 
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
+        
+        String[] imgExt = {".png",".jpeg",".gif", ".csv", ".jpg", ".svg", ".pdf"};
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
+        int nextOpenBracket = markdown.indexOf("[", currentIndex);
+        int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+        int openParen = markdown.indexOf("(", nextCloseBracket);
+        int closeParen = markdown.indexOf(")", openParen);
         while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (nextCloseBracket > openParen){
+                break;
+            }
+            nextOpenBracket = markdown.indexOf("[", currentIndex);
+            nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            openParen = markdown.indexOf("(", nextCloseBracket);
+            closeParen = markdown.indexOf(")", openParen);
+            String appendString = markdown.substring(openParen + 1, closeParen);
+            toReturn.add(appendString);
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
         }
         return toReturn;
+    }
+
+    public boolean containsImgExt(String appendString){
+        return true;
     }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
