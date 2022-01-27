bcefd04b2e936c485678fc6dc77b791977054e01
Ben <benjx32@gmail.com>
2022-01-27 15:39:57 -0800

    junit fixed

--- MarkDownParseTest.java
+++ MarkDownParseTest.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class MarkDownParseTest {
+public class MarkdownParseTest {
     @Test
     public void addition(){
         assertEquals(2, 1+1);
