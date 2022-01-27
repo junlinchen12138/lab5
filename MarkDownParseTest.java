eb912a11508c52073db035c6bc82f6a82bb344c4
Ben <benjx32@gmail.com>
2022-01-27 15:41:00 -0800

    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest //java -cp ".;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkDownParseTest.java
+++ MarkDownParseTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class MarkdownParseTest {
+    // Set command to 4.12
     @Test
     public void addition(){
         assertEquals(2, 1+1);
